55a1e7c53d223746147af4d9e0e96e42fb176cc6
Leaf217 <leaf_217@163.com>
2018-04-06 19:46:00 +0800

    v0406

---
+++ front-end/1 数组去重.js
@@ -0,0 +1,20 @@
+Array.prototype.uniq = function () {
+    //垃圾！！！不支持ES6语法，调了半天！！！
+    //let set = new Set(this);
+    //return [...set];
+    var newArr = [];
+    var flag = true;
+    for (var i = 0;i < this.length;i++) {
+        if (newArr.indexOf(this[i]) == -1){
+            if (this[i] != this[i] && typeof this[i] === 'number') {
+                if (flag) {
+                    newArr.push(this[i]);
+                    flag = false;
+                }
+            } else {
+                newArr.push(this[i]);
+            }
+        }
+    }
+    return newArr;
+}
